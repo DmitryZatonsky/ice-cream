930a2fb389beb55d89848ed95c38b6eadd3fecf5
IhorStorozhok <storozhokigor@gmail.com>
2021-07-22 03:08:10 +0300

    prod

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './sass/main.scss';
 // mobile menu
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.menu-open'),
+    openMenuBtn: document.querySelector('.burger__button'),
     closeMenuBtn: document.querySelector('.menu-close'),
     menu: document.querySelector('.mobile-menu'),
   };
@@ -13,5 +13,20 @@ import './sass/main.scss';
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+  }
+})();
+// about menu
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('.about-open-button'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
